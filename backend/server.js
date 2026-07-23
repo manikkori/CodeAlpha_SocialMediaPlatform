c0fb9ed1fec16666a6198740fe16bafeb4c1271f
Manik kori <manikkori697@gmail.com>
2026-07-23 14:04:45 +0530

    Added auth routes, controller and middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 connectDB();
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("Social Media Backend is LIVE and Connected!");
