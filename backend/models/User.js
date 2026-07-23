9fa2a09bd3d268a6c7524bf9f07c091e299fa06e
Manik kori <manikkori697@gmail.com>
2026-07-23 14:34:44 +0530

    Added user and post models

---
+++ backend/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "Hey there! I am using SocialMedia.",
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
