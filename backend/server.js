cd78aeb5d959ec917c1617bc66658cb46e60d0dd
Manik kori <manikkori697@gmail.com>
2026-07-23 14:12:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,17 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("Social Media Backend is LIVE and Connected!");
